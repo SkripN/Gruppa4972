850b98dc26ea3789fb24097868c9881c8833a9ed
Your Name <you@example.com>
2023-09-12 01:44:05 +0700

    Semenar2 ДЗ

---
+++ Sem2Task10/Program.cs
@@ -0,0 +1,7 @@
+﻿// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,12 @@
+﻿// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
